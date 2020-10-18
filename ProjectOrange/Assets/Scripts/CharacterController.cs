a45a4ef3a6257527771c61a1746018a0ba4e4477
James Dyer <jamesdyer199@gmail.com>
2020-10-19 00:40:29 +1100

    Implement player jumping

--- ProjectOrange/Assets/Scripts/CharacterController.cs
+++ ProjectOrange/Assets/Scripts/CharacterController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 /* Adapted from: https://github.com/jiankaiwang/FirstPersonController */
 
 public class CharacterController : MonoBehaviour {
-    public float speed, ballHeightDiff;
+    public float speed, jumpSpeed, ballHeightDiff;
     public Vector3 ballBottomPos;
     private float translation, strafe;
+    private bool grounded = false;
     private List<Ball> balls = new List<Ball>();
+
 
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
@@ -20,10 +22,18 @@ public class CharacterController : MonoBehaviour {
         translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         transform.Translate(strafe, 0, translation);
+        // FIXME: ^ is it ok to be modifying the transform directly here
+        // when we have a RigidBody?
 
-        if (Input.GetKeyDown("escape")) {
-            // turn on the cursor
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            // Turn on the cursor
             Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) {
+            // Jump by adding force to the RigidBody (so we handle gravity)
+            var rigidBody = GetComponent<Rigidbody>();
+            rigidBody.AddForce(jumpSpeed * Vector3.up, ForceMode.Impulse);
         }
     }
 
@@ -31,5 +41,17 @@ public class CharacterController : MonoBehaviour {
         ball.transform.parent = transform;
         ball.Target = ballBottomPos + Vector3.up * balls.Count * ballHeightDiff;
         balls.Add(ball);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.name == "Ground") {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision){
+        if (collision.gameObject.name == "Ground") {
+            grounded = false;
+        }
     }
 }
