3cd22fd9f057b2abf0cd447ae6965811bc6854da
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-19 22:47:29 +1100

    Add Pulse effect

--- ProjectOrange/Assets/Scripts/ScoreSystem.cs
+++ ProjectOrange/Assets/Scripts/ScoreSystem.cs
@@ -30,12 +30,25 @@ public class ScoreSystem : MonoBehaviour
 
     static public void UpdateScore(int delta) {
         instance.StartCoroutine(instance.UpdateScoreSlowly(delta));
+        instance.StartCoroutine(instance.PulseScore());
     }
 
     private IEnumerator UpdateScoreSlowly(int delta) {
         for (int i = 0; i < delta; i++) {
             score++;
             yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private IEnumerator PulseScore() {
+        for (float i = 1f; i <= 1.5f; i += 0.01f) {
+            scoreText.rectTransform.localScale = new Vector3(i, i, i);
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        for (float i = 1.5f; i >= 1f; i -= 0.01f) {
+            scoreText.rectTransform.localScale = new Vector3(i, i, i);
+            yield return new WaitForSeconds(0.001f);
         }
     }
 }
