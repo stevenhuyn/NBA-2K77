f82b59970c59f5537f44687f47c0c80a699aa0f7
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-19 22:05:01 +1100

    Attatch simple scoring UI

---
+++ ProjectOrange/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public Transform player;
+    public TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float score = player.position.y - 2;
+        scoreText.text = score.ToString("0");
+    }
+}
