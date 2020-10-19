754daa36318f1cc5cc28eb55b5103fbbda2cfb0e
Steven Nguyen <stevenhuyn@gmail.com>
2020-10-19 22:40:16 +1100

    Add static ScoreSystem methods

--- ProjectOrange/Assets/Scripts/ScoreSystem.cs
+++ ProjectOrange/Assets/Scripts/ScoreSystem.cs
@@ -6,35 +6,34 @@ using UnityEngine.UI;
 
 public class ScoreSystem : MonoBehaviour
 {
+    static public ScoreSystem instance;
     public Transform player;
     public TextMeshProUGUI scoreText;
 
     private int score = 0;
 
-    private int multiplier = 0;
+    // Called when an instance awakes in the game
+    void Awake() {
+        instance = this;
+    }
 
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         scoreText.text = score.ToString("0");
     }
 
-    public void UpdateScore(int delta)
-    {
-        StartCoroutine(UpdateScoreSlowly(delta));
+    static public void UpdateScore(int delta) {
+        instance.StartCoroutine(instance.UpdateScoreSlowly(delta));
     }
 
-    private IEnumerator UpdateScoreSlowly(int delta)
-    {
-        for (int i = 0; i < delta; i++)
-        {
+    private IEnumerator UpdateScoreSlowly(int delta) {
+        for (int i = 0; i < delta; i++) {
             score++;
             yield return new WaitForSeconds(0.01f);
         }
