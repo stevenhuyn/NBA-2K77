64a90fae5c127cc81f8cff78269944a21888c24a
James Dyer <jamesdyer199@gmail.com>
2020-10-20 00:22:11 +1100

    Actually pull balls using the hook instead of setting their Target

--- ProjectOrange/Assets/Scripts/GrapplingHook.cs
+++ ProjectOrange/Assets/Scripts/GrapplingHook.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour {
-    public float shootSpeed = 40, playerPullSpeed = 300;
+    public float shootSpeed = 40, playerPullSpeed = 600, ballPullSpeed = 1700;
     public GrappleGun Gun { get; set; }
 
     private bool stuck = false;
+    private Ball ball = null;
     private LineRenderer line = null;
 
     void Start() {
@@ -19,6 +20,12 @@ public class GrapplingHook : MonoBehaviour {
             // Pull on the player until they reach us
             Vector3 dir = (transform.position - Gun.Player.transform.position).normalized;
             Gun.Player.GetComponent<Rigidbody>().AddForce(dir * playerPullSpeed * Time.deltaTime);
+        } else if (ball) {
+            // Move back towards the gun, pulling on the ball
+            Vector3 dir = (Gun.Player.transform.position - transform.position).normalized;
+            var rigidbody = GetComponent<Rigidbody>();
+            rigidbody.AddForce(dir * ballPullSpeed * Time.deltaTime);
+            ball.transform.position += rigidbody.velocity * Time.deltaTime;
         }
 
         // Draw the line to the gun
@@ -26,17 +33,23 @@ public class GrapplingHook : MonoBehaviour {
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Surface")) {
-            // Stick into the wall and begin pulling on the player
-            stuck = true;
-            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            Gun.Player.GetComponent<Rigidbody>().useGravity = false;
-        } else if (other.CompareTag("Ball")) {
-            // Pull the ball towards the player
-            other.GetComponent<Ball>().Target = Gun.Player.transform.position;
+        if (!stuck && !ball) {
+            if (other.CompareTag("Surface")) {
+                // Stick into the wall and begin pulling on the player
+                stuck = true;
+                GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                Gun.Player.GetComponent<Rigidbody>().useGravity = false;
+            } else if (other.CompareTag("Ball")) {
+                // Pull the ball towards the player
+                ball = other.GetComponent<Ball>();
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
         } else if (Gun.Player.gameObject == other.gameObject) {
             // Self-destruct when touched by the player
             Destroy(gameObject);
+            if (ball) {
+                Gun.Player.GiveBall(ball);
+            }
         }
     }
 }
