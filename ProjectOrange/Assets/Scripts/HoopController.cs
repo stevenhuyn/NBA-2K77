e9c94e0239f6f193ec59cb26bc6c4c613610df19
Gatlee Kaw <gatlee.kaw@gmail.com>
2020-10-24 22:13:52 +1100

    Add audio

--- ProjectOrange/Assets/Scripts/HoopController.cs
+++ ProjectOrange/Assets/Scripts/HoopController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class HoopController : MonoBehaviour {
     public new ParticleSystem particleSystem;
-
+    public AudioSource audioSource;
+    public AudioClip explosion;
     public void HandleDunk(List<Ball> balls) {
         ParticleSystem particles;
         particles = Object.Instantiate(particleSystem, gameObject.transform.position, Quaternion.identity);
         particles.transform.SetParent(transform);
         particles.Play();
+
+        AudioSource.PlayClipAtPoint(explosion, transform.position, 0.5f);
     }
 }
