14280f19ce3b36e5b773c3918b0ca67499413ee9
James Dyer <jamesdyer199@gmail.com>
2020-11-03 00:53:37 +1100

    Add smooth gun rotation

--- ProjectOrange/Assets/Scripts/GrappleGun.cs
+++ ProjectOrange/Assets/Scripts/GrappleGun.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class GrappleGun : MonoBehaviour {
     public GameObject grapplingHookPrefab;
+    public float rotationSpeed = 2;
     public CharacterController Player { get; private set; }
     public GameObject Hook { get; private set; }
+
+    private Quaternion targetRotation;
 
     void Start() {
         Player = GetComponentInParent<CharacterController>();
@@ -18,14 +21,24 @@ public class GrappleGun : MonoBehaviour {
             Hook = Instantiate(grapplingHookPrefab);
             Hook.GetComponent<GrapplingHook>().Gun = this;
             Hook.transform.position = Player.transform.position;
-            Hook.transform.rotation = transform.rotation;
+            Hook.transform.rotation = Camera.main.transform.rotation;
+            Hook.transform.Rotate(Vector3.right, 90);
         } else if (Input.GetKeyUp(KeyCode.Mouse0)) {
             // Destroy the Hook if left click is released
             DestroyHook();
         }
-        if (!Hook) {
+        if (Hook) {
+            targetRotation = Quaternion.LookRotation(Hook.transform.position - transform.position);
+        } else {
             Player.GetComponent<Rigidbody>().useGravity = true;
+            targetRotation = Camera.main.transform.rotation;
         }
+
+        // Apply the rotation
+        // Have to rotate 90 degrees to the right because we use UP us the main direction, not FORWARD
+        transform.Rotate(-Vector3.right, 90);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
+        transform.Rotate(Vector3.right, 90);
     }
 
     public void DestroyHook() {
