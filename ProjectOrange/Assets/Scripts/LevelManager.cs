4f8f1ac72d76c5b5a2d70149299a1769a668b5b2
Steven Nguyen <stevenhuyn@gmail.com>
2020-11-02 15:33:50 +1100

    Add level restarting with R

--- ProjectOrange/Assets/Scripts/LevelManager.cs
+++ ProjectOrange/Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.R)) {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
     }
 }
