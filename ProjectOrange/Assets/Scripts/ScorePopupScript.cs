035db309c63a588f91035e60a178887d7a55b644
Steven Nguyen <stevenhuyn@gmail.com>
2020-11-03 23:21:16 +1100

    Move scorepopup upwards in frame

--- ProjectOrange/Assets/Scripts/ScorePopupScript.cs
+++ ProjectOrange/Assets/Scripts/ScorePopupScript.cs
@@ -12,10 +12,13 @@ public class ScorePopupScript : MonoBehaviour
 
     private int timeAlive;
 
+    private TextMeshProUGUI tmp;
+
     // Start is called before the first frame update
     void Start()
     {
         timeAlive = 0;
+        tmp = transform.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -26,7 +29,8 @@ public class ScorePopupScript : MonoBehaviour
 
     void FixedUpdate() {
         timeAlive++;
-        transform.GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, 1.0f - (float) timeAlive / lifeLength);
+        tmp.color = new Color(1, 1, 1, 1.0f - (float) timeAlive / lifeLength);
+        tmp.rectTransform.localPosition += new Vector3(0, 1f, 0);
         if (timeAlive >= lifeLength) {
             Destroy(this.gameObject);
         }
