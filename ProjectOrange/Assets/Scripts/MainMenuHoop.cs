4626b597f609926d315d3ff9927f2c3a85b9bbef
Steven Nguyen <stevenhuyn@gmail.com>
2020-11-04 22:53:03 +1100

    Add main menu scene mockup

---
+++ ProjectOrange/Assets/Scripts/MainMenuHoop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuHoop : MonoBehaviour
+{
+    private Vector3 startingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startingPosition + Mathf.Sin(Time.time / 2f) * Vector3.up * 0.1f;
+    }
+}
