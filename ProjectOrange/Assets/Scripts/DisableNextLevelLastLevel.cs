20dedd2bb033f16333885ae39124d1496f0685b5
Steven Nguyen <stevenhuyn@gmail.com>
2020-11-06 10:25:49 +1100

    Handle disable next Level button on last level

---
+++ ProjectOrange/Assets/Scripts/DisableNextLevelLastLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableNextLevelLastLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate() {
+        gameObject.GetComponent<Button>().interactable = LevelManager.level != 6;
+    }
+}
