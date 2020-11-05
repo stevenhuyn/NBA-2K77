6c3a1bea28065e818dc036937a1e3fbd353da58f
Steven Nguyen <stevenhuyn@gmail.com>
2020-11-05 11:34:41 +1100

    Hookup basic buttons

--- ProjectOrange/Assets/Scripts/MainMenuButtons.cs
+++ ProjectOrange/Assets/Scripts/MainMenuButtons.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuHoop : MonoBehaviour
+public class MainMenuButtons : MonoBehaviour
 {
-    private Vector3 startingPosition;
     // Start is called before the first frame update
     void Start()
     {
-        startingPosition = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = startingPosition + Mathf.Sin(Time.time / 2f) * Vector3.up * 0.1f;
+
     }
 
     public void PressPlay() {
         SceneManager.LoadScene("lvl1");
+    }
+
+
+    public void Credits() {
+        Debug.Log("NBA 2K");
+    }
+
+    public void Exit() {
+        Application.Quit();
     }
 }
