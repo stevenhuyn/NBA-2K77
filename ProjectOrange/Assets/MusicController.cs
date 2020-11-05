d80a5ac97a2e9b1c3230382e09a9c8892211e85e
Gatlee <gatmek1@gmail.com>
2020-11-06 01:18:22 +1100

    Add music to game

---
+++ ProjectOrange/Assets/MusicController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public static MusicController instance;
+    // Start is called before the first frame update
+    void Awake() {
+        // Ensures this is a singleton
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+
+        // Ensures that this keeps playing between scenes
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
